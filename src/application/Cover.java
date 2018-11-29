5b8be7557f9152b74493ea40524575e73f9b6dfd
Janejira <janejira885@gmail.com>
2018-11-29 20:30:56 +0700

    Cover.java

---
+++ src/application/Cover.java
@@ -0,0 +1,67 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class Cover extends Application {
+	
+	private Scene sceneMenu, sceneSelect, sceneLevel;
+	private MenuScene menu;
+	private LevelScene level;
+	private SelectMode select;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		menu = new MenuScene(); sceneMenu = new Scene(menu,900,600);
+		select = new SelectMode(); sceneSelect = new Scene(select,900,600);
+		level = new LevelScene(); sceneLevel = new Scene(level,900,600);
+		
+		
+		((Button)menu.getStartBtn()).setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				stage.setScene(sceneSelect);
+			}
+		});
+		
+		((Button)select.getStMode()).setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.setScene(sceneLevel);
+			}
+		});
+		
+		((Button)select.getBack()).setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.setScene(sceneMenu);
+			}
+		});
+		
+		((Button)level.getBack()).setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.setScene(sceneSelect);
+			}
+		});
+		
+		stage.setTitle("I Love Prog Meth");
+		stage.setScene(sceneMenu);
+		stage.show();
+		
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+}
+
+
